9321804c8dde057bb1d22886a9fc5be04f33d83f
Brian Yu <brianyu28@gmail.com>
2021-05-14 00:55:52 -0400

    Initial Commit

---
+++ Shared/MoonwalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonwalkApp.swift
+//  Shared
+//
+//  Created by Brian Yu on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonwalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
